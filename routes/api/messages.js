62ae93b863f692d305ccd62b15a6a6b7aa5dd638
Zach Harrison <zharrison@shaw.ca>
2021-04-04 11:27:23 -0700

    refactored messages post route with async await

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
-const checkObjectId = require('../../middleware/checkObjectId');
 
 const Message = require('../../models/Message');
 const Conversation = require('../../models/Conversation');
@@ -12,54 +11,35 @@ router.post('/', auth, async (req, res) => {
   const from = req.user.id;
   const to = req.body.to;
 
-  await Conversation.findOneAndUpdate(
-    {
-      recipients: {
-        $all: [{ $elemMatch: { $eq: from } }, { $elemMatch: { $eq: to } }],
+  try {
+    const conversation = await Conversation.findOneAndUpdate(
+      {
+        recipients: {
+          $all: [{ $elemMatch: { $eq: from } }, { $elemMatch: { $eq: to } }],
+        },
       },
-    },
-    {
-      recipients: [from, to],
-      lastMessage: req.body.text,
-      date: Date.now(),
-    },
-    { upsert: true, new: true, setDefaultsOnInsert: true },
-    function (err, conversation) {
-      if (err) {
-        console.log(err);
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ message: 'Failure' }));
-        res.sendStatus(500);
-      } else {
-        let message = new Message({
-          conversation: conversation._id,
-          to,
-          from,
-          text: req.body.text,
-        });
+      {
+        recipients: [from, to],
+        lastMessage: req.body.text,
+        date: Date.now(),
+      },
+      { upsert: true, new: true, setDefaultsOnInsert: true }
+    );
 
-        // req.io.sockets.emit('messages', req.body.body);
+    const newMessage = new Message({
+      conversation: conversation._id,
+      to,
+      from,
+      text: req.body.text,
+    });
 
-        message.save((err) => {
-          if (err) {
-            console.log(err);
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({ message: 'Failure' }));
-            res.sendStatus(500);
-          } else {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(message);
-            res.end(
-              JSON.stringify({
-                message: 'Success',
-                conversationId: conversation._id,
-              })
-            );
-          }
-        });
-      }
-    }
-  );
+    const message = await newMessage.save();
+
+    res.json(message);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
