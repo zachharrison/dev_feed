5477982d6e42b96687f0e1b584d0d1014027e95a
Zach Harrison <zharrison@shaw.ca>
2021-04-04 16:08:35 -0700

    Trying to fix new conversation bug

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -10,35 +10,102 @@ const Conversation = require('../../models/Conversation');
 // @DESC      CREATE A NEW MESSAGE
 // @ROUTE     POST /api/messages
 // @ACCESS    PRIVATE
+
+/********************************************************
+  CURRENT STATUS: New messages are being created
+  and messages are not. This is causing the code
+  to create a new conversation everytime a message
+  is sent, even if we already have an 
+  existing chat with the to and from users.
+
+  SOLUTIONS: Maybe look for a different 
+  mongoDB method which checks if table
+  exists
+***********************************************************/
+
 router.post('/', auth, async (req, res) => {
-  const from = req.user.id;
+  const from = req.body.from;
   const to = req.body.to;
-
+  // console.log(req.body);
+  // console.log(req.user.id);
+  // console.log('from id, ', from);
+  // console.log('to id, ', to);
   try {
-    const conversation = await Conversation.findOneAndUpdate(
-      {
-        recipients: {
-          $all: [{ $elemMatch: { $eq: from } }, { $elemMatch: { $eq: to } }],
-        },
+    const conversation = await Conversation.findOne({
+      recipients: {
+        $all: [from, to],
       },
-      {
-        recipients: [from, to],
-        lastMessage: req.body.text,
-        date: Date.now(),
-      },
-      { upsert: true, new: true, setDefaultsOnInsert: true }
-    );
-
-    const newMessage = new Message({
-      conversation: conversation._id,
-      to,
-      from,
-      text: req.body.text,
     });
 
-    const message = await newMessage.save();
+    // console.log(conversation);
 
-    res.json(message);
+    if (!conversation) {
+      console.log('created new conversation');
+      const conversation = await new Conversation({
+        recipients: [from, to],
+        lastMessage: req.body.text,
+      });
+
+      const newMessage = new Message({
+        conversation: conversation._id,
+        to,
+        from,
+        text: req.body.text,
+      });
+
+      const message = await newMessage.save();
+
+      res.json(message);
+    } else {
+      console.log('found conversation');
+      const conversation = await Conversation.findOneAndUpdate(
+        {
+          // recipients: {
+          //   $all: [{ $elemMatch: { $eq: from } }, { $elemMatch: { $eq: to } }],
+          // },
+          recipients: {
+            $all: [from, to],
+          },
+        },
+        {
+          recipients: [from, to],
+          lastMessage: req.body.text,
+          date: Date.now(),
+        },
+        { upsert: true, setDefaultsOnInsert: true }
+      );
+      const newMessage = new Message({
+        conversation: conversation._id,
+        to,
+        from,
+        text: req.body.text,
+      });
+
+      const message = await newMessage.save();
+
+      res.json(message);
+    }
+
+    // res.json(conversation);
+    // const conversation = await Conversation.findOneAndUpdate(
+    //   {
+    //     recipients: {
+    //       $all: [{ $elemMatch: { $eq: from } }, { $elemMatch: { $eq: to } }],
+    //     },
+    //     recipients: {
+    //       $all: [from, to],
+    //     },
+    //   },
+    //   {
+    //     recipients: [from, to],
+    //     lastMessage: req.body.text,
+    //     date: Date.now(),
+    //   },
+    //   { upsert: true, new: true, setDefaultsOnInsert: true }
+    //   { upsert: true, setDefaultsOnInsert: true }
+    // );
+
+    // res.json(newMessage);
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Server Error');
