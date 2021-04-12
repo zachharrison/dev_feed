8ec1297e8dbb3623ffe1f226a8b70c98e414d863
Zach Harrison <zharrison@shaw.ca>
2021-04-12 12:33:20 -0700

    Adjust Profile Schema to take an image

--- models/Profile.js
+++ models/Profile.js
@@ -6,6 +6,9 @@ const ProfileSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
   },
+  image: {
+    type: String,
+  },
   company: {
     type: String,
   },
