022738742f5c37ec04586ed78a518141a9c7c702
Zach Harrison <zharrison@shaw.ca>
2021-04-12 18:23:16 -0700

    Adjusted App.js classes so profile page looks as expected

--- client/src/App.js
+++ client/src/App.js
@@ -37,34 +37,37 @@ const App = () => {
         <>
           <Navbar />
           <Route exact path='/' component={Landing} />
-          <section className='container container-center'>
+          <section className='container'>
             <Alert />
             <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/profiles' component={Profiles} />
               <Route exact path='/profile/:id' component={Profile} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-              <PrivateRoute
-                exact
-                path='/create-profile'
-                component={ProfileForm}
-              />
-              <PrivateRoute
-                exact
-                path='/edit-profile'
-                component={ProfileForm}
-              />
-              <PrivateRoute
-                exact
-                path='/add-experience'
-                component={AddExperience}
-              />
-              <PrivateRoute
-                exact
-                path='/add-education'
-                component={AddEducation}
-              />
+
+              <div className='container-center'>
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/profiles' component={Profiles} />
+                <PrivateRoute exact path='/dashboard' component={Dashboard} />
+                <PrivateRoute
+                  exact
+                  path='/create-profile'
+                  component={ProfileForm}
+                />
+                <PrivateRoute
+                  exact
+                  path='/edit-profile'
+                  component={ProfileForm}
+                />
+                <PrivateRoute
+                  exact
+                  path='/add-experience'
+                  component={AddExperience}
+                />
+                <PrivateRoute
+                  exact
+                  path='/add-education'
+                  component={AddEducation}
+                />
+              </div>
             </Switch>
           </section>
         </>
--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
 import { getProfileById } from '../../actions/profile';
+import { Link } from 'react-router-dom';
 
 const Profile = ({
   getProfileById,
@@ -12,10 +14,33 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-    // eslint-disable-next-line
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
-  return <>{profile === null || loading ? <Spinner /> : <>Profile</>}</>;
+  return (
+    <>
+      {profile === null ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className='form-group-row'>
+            <Link to='/profiles' className='btn btn-light'>
+              Back To Profiles
+            </Link>
+            {auth.isAuthenticated &&
+              auth.loading === false &&
+              auth.user._id === profile.user._id && (
+                <Link to='/edit-profile' className='btn btn-dark'>
+                  Edit Profile
+                </Link>
+              )}
+          </div>
+          <div className='profile-grid my-1'>
+            <ProfileTop profile={profile} />
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 Profile.propTypes = {
