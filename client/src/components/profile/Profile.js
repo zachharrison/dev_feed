bd517860e98c39dddcd7cf0ad49dc9124ebb6012
Zach Harrison <zharrison@shaw.ca>
2021-04-13 09:10:35 -0700

    Added ProfileExperience component to Profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
 import { getProfileById } from '../../actions/profile';
 import { Link } from 'react-router-dom';
 
@@ -38,6 +39,21 @@ const Profile = ({
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+            <div className='profile-exp bg-white p-2'>
+              <h2 className='text-primary'>Experience</h2>
+              {profile.experience.length > 0 ? (
+                <>
+                  {profile.experience.map((experience) => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    />
+                  ))}
+                </>
+              ) : (
+                <h4>No experience yet</h4>
+              )}
+            </div>
           </div>
         </>
       )}
