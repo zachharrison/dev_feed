bb4d1c209df0e6da9418bad2bda08b03a8658092
Zach Harrison <zharrison@shaw.ca>
2021-04-12 17:13:13 -0700

    Added profile component and loading the correct profile in state on view profile click

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import ProfileForm from './components/profile-forms/ProfileForm';
 import AddEducation from './components/profile-forms/AddEducation';
 import AddExperience from './components/profile-forms/AddExperience';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 
 // REDUX
 import { Provider } from 'react-redux';
@@ -42,6 +43,7 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:id' component={Profile} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute
                 exact
---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getProfileById } from '../../actions/profile';
+
+const Profile = ({
+  getProfileById,
+  profile: { profile, loading },
+  auth,
+  match,
+}) => {
+  useEffect(() => {
+    getProfileById(match.params.id);
+    // eslint-disable-next-line
+  }, [getProfileById]);
+
+  return <>{profile === null || loading ? <Spinner /> : <>Profile</>}</>;
+};
+
+Profile.propTypes = {
+  getProfileById: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { getProfileById })(Profile);
