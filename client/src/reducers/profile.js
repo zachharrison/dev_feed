416aeceaaa3731f84c122614f5fe0589690685b6
Zach Harrison <zharrison@shaw.ca>
2021-04-13 10:36:09 -0700

    Fix registration bug

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -38,6 +38,7 @@ export default function profileReducer(state = initialState, action) {
         ...state,
         error: payload,
         loading: false,
+        profile: null,
       };
 
     case CLEAR_PROFILE:
